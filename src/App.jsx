5961b931781cddda475e0b70fef6222c96d90634
samadhan <samadhanbhusnar21@gmail.com>
2025-12-12 19:10:39 +0530

    started pushing code to github from musify-app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-gradient-to-br from-purple-600 to-blue-500 text-white text-3xl font-bold">
+        <h1>Musify App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
